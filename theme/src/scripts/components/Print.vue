45f092ace6d39417205350f1b58e5efe07c0b919
Fred Every <fred@fredevery.co.za>
2021-04-23 21:50:57 +0200

    Print section refinements - 1st pass

--- theme/src/scripts/components/Print.vue
+++ theme/src/scripts/components/Print.vue
@@ -5,24 +5,23 @@
     :class="sectionClasses"
     :style="sectionStyles"
   >
-   
     <div class="grid">
       <div class="content grid__column--12">
         <div class="section__copy">
+          <h1 class="title">PRINT</h1>
+          <div class="print-head">
+            <div class="filament" />
+          </div>
+          <div class="copy">
+            <p class="grid__column--3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
 
-          <h1 class="pront content grid__row--4 print">PRINT</h1>
-
-           <p class="para content grid__column--3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-
-          <p class="para content grid__column--6"> </p>
-         
-          <p class="para paraa content grid__column--3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+            <p class="grid__column--3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
         </div>
-       
       </div>
     </div>
   </section>
@@ -32,7 +31,9 @@
       ref="background"
       class="print__background section-background"
       :style="sectionBackgroundStyles"
-    />
+    >
+      <div class="build-plate-grid" />
+    </div>
   </teleport>
 </template>
 
@@ -48,8 +49,8 @@ export default {
         stage: {
           drone: {
             appearance: 'shaded',
-            rotation: ['40deg', '140deg', '0deg'],
-            position: [0, 0, 0],
+            rotation: ['0deg', '540deg', '180deg'],
+            position: [0, window.innerHeight * -0.25, 0],
           }
         }
       }
