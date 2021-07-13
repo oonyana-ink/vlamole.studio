59317d4d9f02c6e21f2a4cb28944e12b5ecdd847
Juan Levin <jwanchino@gmail.com>
2021-07-13 10:59:34 +0200

    WIP

--- theme/src/scripts/App.vue
+++ theme/src/scripts/App.vue
@@ -15,6 +15,7 @@ import Nopage from '@components/Nopage.vue'
 import Scrollbar from '@components/Scrollbar.vue'
 import ProductSelector from '@components/ProductSelector.vue'
 
+
 const pageLoaders = Object.entries(pages).map(([path, component]) => {
   const pathRegex = new RegExp(`^${path.replace(/\//g, '\/')}`)
   return { test: () => pathRegex.test(location.pathname), path, component }
